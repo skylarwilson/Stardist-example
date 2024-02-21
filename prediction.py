7b08fc81f9172b0d551217ae4425fd4ee5d97c24
Skylar Wilson <136635193+skylarwilson@users.noreply.github.com>
2024-02-20 21:16:37 -0600

    updated stuff

--- prediction.py
+++ prediction.py
@@ -21,7 +21,7 @@ np.random.seed(42)
 lbl_cmap = random_label_cmap()
 
 # directory containing images for prediction
-image_dir = "/content/Stardist-example/"
+image_dir = "/content/Stardist-example/predictionimages"
 
 # sorts files by name
 file_names = sorted(os.listdir(image_dir))
